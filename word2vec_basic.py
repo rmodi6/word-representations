65d012e0f133e7f94e2280e9da7d158b9c18354e
Ruchit Modi <modi.ruchit6@gmail.com>
2019-10-02 13:39:48 -0400

    Added predictions of word analogy task for model with nce loss

--- word2vec_basic.py
+++ word2vec_basic.py
@@ -172,28 +172,6 @@ def generate_batch(data, batch_size, num_skips, skip_window):
     data_index += 1
   return batch, labels
 
-  # span = 2 * skip_window + 1  # [ skip_window target skip_window ]
-  # buffer = collections.deque(maxlen=span)  # pylint: disable=redefined-builtin
-  # if data_index + span > len(data):
-  #   data_index = 0
-  # buffer.extend(data[data_index:data_index + span])
-  # data_index += span
-  # for i in range(batch_size // num_skips):
-  #   context_words = [w for w in range(span) if w != skip_window]
-  #   words_to_use = random.sample(context_words, num_skips)
-  #   for j, context_word in enumerate(words_to_use):
-  #     batch[i * num_skips + j] = buffer[skip_window]
-  #     labels[i * num_skips + j, 0] = buffer[context_word]
-  #   if data_index == len(data):
-  #     buffer.extend(data[0:span])
-  #     data_index = span
-  #   else:
-  #     buffer.append(data[data_index])
-  #     data_index += 1
-  # # Backtrack a little bit to avoid skipping words in the end of a batch
-  # data_index = (data_index + len(data) - span) % len(data)
-  # return batch, labels
-
 
 def build_model(sess, graph, loss_model):
   """
